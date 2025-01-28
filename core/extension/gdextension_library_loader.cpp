migeran/godot_staging

GDExtension: Use loader to check if the library exists.

core/extension/gdextension_library_loader.cpp
@@ -268,6 +268,10 @@ bool GDExtensionLibraryLoader::has_library_changed() const {
 	return false;
 }
 
+bool GDExtensionLibraryLoader::is_library_existing() const {
+	return FileAccess::exists(resource_path);
+}
+
 Error GDExtensionLibraryLoader::parse_gdextension_file(const String &p_path) {
 	resource_path = p_path;
 

core/extension/gdextension_loader.h
@@ -42,6 +42,7 @@ public:
 	virtual void close_library() = 0;
 	virtual bool is_library_open() const = 0;
 	virtual bool has_library_changed() const = 0;
+	virtual bool is_library_existing() const = 0;
 };
 
 #endif // GDEXTENSION_LOADER_H

metamuffin/godot

core/extension/gdextension_library_loader.cpp
@@ -259,6 +259,10 @@ bool GDExtensionLibraryLoader::has_library_changed() const {
 	return false;
 }
 
+bool GDExtensionLibraryLoader::library_exists() const {
+	return FileAccess::exists(resource_path);
+}
+
 Error GDExtensionLibraryLoader::parse_gdextension_file(const String &p_path) {
 	resource_path = p_path;
 

core/extension/gdextension_loader.h
@@ -42,6 +42,7 @@ public:
 	virtual void close_library() = 0;
 	virtual bool is_library_open() const = 0;
 	virtual bool has_library_changed() const = 0;
+	virtual bool library_exists() const = 0;
 };
 
 #endif // GDEXTENSION_LOADER_H